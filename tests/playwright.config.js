7da90f1c97ed6f0b21e7208066d23c29fb9b7988
OhadAsh <63406525+OhadAsh@users.noreply.github.com>
2026-04-09 18:24:00 +0300

    Adjust CI to see run time

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   testDir: 'specs',
   testMatch: ['**/*.spec.js'],
   retries: 1,
-  workers: 4,
+  workers: process.env.CI ? 2 : 4,
   // Global per-test timeout.
   timeout: 360_000,
   outputDir: 'test-results',
